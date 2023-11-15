f6c4518e14a266573e563023b9554120b4296e7a
praveen <praveendunna19@gmail.com>
2023-11-15 10:03:08 +0530

    navbar

---
+++ praveen/src/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='header'>
+    <div className ="logo">
+     <p>Navbar</p> 
+     </div>
+     <div className='navbar'>
+       <ul>
+         <li>home</li>
+         <li>about</li>
+         <li>service</li>
+         <li>contact</li>
+       </ul>
+       </div>     
+   </div>
+  )
+}
