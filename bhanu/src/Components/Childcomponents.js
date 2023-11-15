c3b3720af40d3cc9964512cde4166c4963c908df
Bhanu <bhanueatcharala@gmail.com>
2023-11-15 10:03:26 +0530

    dashdoard

---
+++ bhanu/src/Components/Childcomponents.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Childcomponents() {
+  return (
+    <div>
+
+    <div>
+    <div className='header'>
+     <div className ="logo">
+      <p>Navbar</p> 
+      </div>
+      <div className='navbar'>
+        <ul>
+          <li>home</li>
+          <li>about</li>
+          <li>services</li>
+          <li>contact</li>
+        </ul>
+        </div>     
+    </div>
+    </div>
+    </div>
+  )
+}
