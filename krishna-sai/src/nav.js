926751f1b44bad51a5ccabc90cb8d150e6c8eb0c
krishna <krishnakirrak@gmail.com>
2023-11-15 09:48:07 +0530

    created nav bar

---
+++ krishna-sai/src/nav.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+
+
+export default function Nav() {
+
+
+
+    return (
+        <>
+            <div className='header'>
+                <div className="logo">
+                    <p>Krishna...</p>
+                </div>
+                <div className='navbar'>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Service</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+
+    )
+}
